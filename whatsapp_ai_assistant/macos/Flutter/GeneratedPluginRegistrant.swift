feafb78bbc8c84964d0ab48f9d193242a5e7e837
Deadshot0362 <bs343333@gmail.com>
2025-06-07 03:55:05 +0530

    feat: add flutter secure storage plugin for all platforms
    Add flutter_secure_storage plugin to enable secure storage functionality across Linux, Windows, and macOS platforms. This includes updating platform-specific configuration files and adding the required dependencies.

--- whatsapp_ai_assistant/macos/Flutter/GeneratedPluginRegistrant.swift
+++ whatsapp_ai_assistant/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import app_settings
+import flutter_secure_storage_macos
 import path_provider_foundation
 import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppSettingsPlugin.register(with: registry.registrar(forPlugin: "AppSettingsPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
 }
